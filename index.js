b7aca5a16597509a52c53b04374a19d82dab837b
Dido Arellano <dido@semicolonstudios.com>
2016-05-20 15:37:19 +0800

    Create and use slug plugin

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
 const layouts = require('metalsmith-layouts')
 const flatten = require('./lib/metalsmith-flatten')
+const slug = require('./lib/metalsmith-slug')
 
 metalsmith(__dirname)
   .use(flatten())
+  .use(slug())
   .use(markdown())
   .use(layouts({ engine: 'pug', default: 'post.pug', pretty: true }))
   .build(function (err) {
