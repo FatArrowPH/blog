66739193341eca2770fbf7a1e38503912616b4b8
Dido Arellano <dido@semicolonstudios.com>
2016-05-20 14:18:54 +0800

    Use layouts w/ pug as template engine

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
+const layouts = require('metalsmith-layouts')
 const flatten = require('./lib/metalsmith-flatten')
 
 metalsmith(__dirname)
   .use(flatten())
   .use(markdown())
+  .use(layouts({ engine: 'pug', default: 'post.pug', pretty: true }))
   .build(function (err) {
     if (err) console.log(err)
   })
