874caa4256a8d379b8470c65af7a029ae35642b6
Dido Arellano <dido@semicolonstudios.com>
2016-05-20 14:02:11 +0800

    Flatten build output dir

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
+const flatten = require('./lib/metalsmith-flatten')
 
 metalsmith(__dirname)
+  .use(flatten())
   .use(markdown())
   .build(function (err) {
     if (err) console.log(err)
