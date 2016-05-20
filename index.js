29a008979cdba2b9982b0a0bdf655b8c538cc6b1
Dido Arellano <dido@semicolonstudios.com>
2016-05-20 15:59:04 +0800

    Humanize dates in yaml front matter

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const markdown = require('metalsmith-markdown')
 const layouts = require('metalsmith-layouts')
 const flatten = require('./lib/metalsmith-flatten')
 const slug = require('./lib/metalsmith-slug')
+const humanizeDate = require('./lib/metalsmith-humanize-date')
 
 metalsmith(__dirname)
   .use(flatten())
   .use(slug())
   .use(markdown())
+  .use(humanizeDate())
   .use(layouts({ engine: 'pug', default: 'post.pug', pretty: true }))
   .build(function (err) {
     if (err) console.log(err)
