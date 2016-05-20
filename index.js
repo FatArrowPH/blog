5a6df6063460720b6dcc38c1e4b3b67988f5c6f1
Dido Arellano <dido@semicolonstudios.com>
2016-05-20 13:24:11 +0800

    Use markdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const metalsmith = require('metalsmith')
+const markdown = require('metalsmith-markdown')
 
 metalsmith(__dirname)
+  .use(markdown())
   .build(function (err) {
     if (err) console.log(err)
   })
